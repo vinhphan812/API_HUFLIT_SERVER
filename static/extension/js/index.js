e2290e1e26da11bcd0248dcd32ece7204eaa12f1
Vinh Phan <vinhphan812@gmail.com>
2020-09-08 22:25:10 +0700

    fix js render Menu img

--- static/extension/js/index.js
+++ static/extension/js/index.js
@@ -4,7 +4,7 @@ var dataMenu = [['Schedules','Schedules'], ['ChangePassword','Change Password'],
 // if done --> DOM data 
 function isDone(data){
      var liMenu = dataMenu.map(function(item){
-          return '<li class="itemMenu" id="' + item[0] + '"><img class="icon" src="/assets/img/' + item[0] + '.png"><span>' + item[1] + '</span></li>'
+          return '<li class="itemMenu" id="' + item[0] + '"><img class="icon" src="https://api-huflit.herokuapp.com/extension/img/' + item[0] + '.png"><span>' + item[1] + '</span></li>'
      })
      if(document.getElementById('loader'))
           stopLoading();
@@ -26,7 +26,7 @@ function isDone(data){
      
 
      //DOM username
-     Menu.innerHTML = '<div class="user flex"><img src="/assets/img/user.png"><p>' + data + '</p></div><ul class="menu flex">' + liMenu.join('') + '</ul>';
+     Menu.innerHTML = '<div class="user flex"><img src="https://api-huflit.herokuapp.com/extension/img/user.png"><p>' + data + '</p></div><ul class="menu flex">' + liMenu.join('') + '</ul>';
      //DOM menu list
      document.getElementById('Schedules').addEventListener('click', Schedule);
      document.getElementById('ChangePassword').addEventListener('click', ChangePassword);
