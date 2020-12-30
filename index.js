a871ac96cae98cee4a66caf1e8b54b341f4a8be7
vinhphan812 <vinhphan812@gmail.com>
2020-12-31 04:10:21 +0700

    update UI homePage 31/12/2020

--- index.js
+++ index.js
@@ -1,73 +1,70 @@
-const HUFLIT = require('./modules/Huflit'),
-     bodyParser = require('body-parser'),
-     express = require('express'),
-     path = require('path'),
-     cors = require('cors'),
-     app = express(),
-     port = 3000,
-     SERVER = 'https://portal.huflit.edu.vn/',
-     API = new HUFLIT();
+const HUFLIT = require("./modules/Huflit"),
+	bodyParser = require("body-parser"),
+	express = require("express"),
+	path = require("path"),
+	cors = require("cors"),
+	app = express(),
+	port = 3000,
+	SERVER = "https://portal.huflit.edu.vn/",
+	API = new HUFLIT();
 
-
-app.set('view engine', 'pug');
-app.set('views', 'static');
+app.set("view engine", "pug");
+app.set("views", "static");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, "static")));
 
-app.get('/', cors(), function(req, res) {
-     res.render('index');
+app.get("/", cors(), function (req, res) {
+	res.render("index");
 });
 
-app.post('/profile', cors(), async(req, res, next) => {
-     try {
-          res.json(await API.login(req.body));
-     } catch (error) {
-          res.send(error);
-     }
+app.post("/profile", cors(), async (req, res, next) => {
+	API.login(req.body)
+		.then((result) => res.json(result))
+		.catch((error) => res.send(error));
 });
 
-app.post('/CheckCookie', cors(), async(req, res, next) => {
-     try {
-          API.jar.setCookie(req.body.cookie, SERVER)
-          res.send(await API.CheckCookie());
-     } catch (error) {
-          res.send(error);
-     }
+app.post("/CheckCookie", cors(), async (req, res, next) => {
+	try {
+		API.jar.setCookie(req.body.cookie, SERVER);
+		res.send(await API.CheckCookie());
+	} catch (error) {
+		res.send(error);
+	}
 });
 
-app.post('/Schedules', cors(), async(req, res, next) => {
-     try {
-          API.jar.setCookie(req.body.cookie, SERVER);
-          var data = await API.getSchedules('HK01');
-          res.send(data);
-     } catch (error) {
-          res.send(error);
-     }
+app.post("/Schedules", cors(), async (req, res, next) => {
+	try {
+		API.jar.setCookie(req.body.cookie, SERVER);
+		var data = await API.getSchedules("HK01");
+		res.send(data);
+	} catch (error) {
+		res.send(error);
+	}
 });
 
-app.post('/ChangePass', cors(), async(req, res, next) => {
-     try {
-          API.jar.setCookie(req.body.cookie, SERVER);
-          res.send(await API.ChangePass(req.body.oldPass, req.body.newPass));
-     } catch (error) {
-          res.send(error);
-     }
+app.post("/ChangePass", cors(), async (req, res, next) => {
+	try {
+		API.jar.setCookie(req.body.cookie, SERVER);
+		res.send(await API.ChangePass(req.body.oldPass, req.body.newPass));
+	} catch (error) {
+		res.send(error);
+	}
 });
 
-app.get('/openServer', cors(), function(req, res) {
-     res.send(true);
-})
+app.get("/openServer", cors(), function (req, res) {
+	res.send(true);
+});
 
-app.post('/fee', cors(), async function(req, res) {
-     try {
-          API.jar.setCookie(req.body.cookie, SERVER);
-          res.send(await API.getFee());
-     } catch (error) {
-          res.send(error);
-     }
-})
+app.post("/fee", cors(), async function (req, res) {
+	try {
+		API.jar.setCookie(req.body.cookie, SERVER);
+		res.send(await API.getFee());
+	} catch (error) {
+		res.send(error);
+	}
+});
 
 app.listen(process.env.PORT || port, () => console.log("Server is running..."));
--- modules/Huflit.js
+++ modules/Huflit.js
@@ -1,103 +1,138 @@
-var request = require('request-promise');
-var cheerio = require('cheerio');
+var request = require("request-promise");
+var cheerio = require("cheerio");
 
-const API_SERVER = 'https://portal.huflit.edu.vn/';
+const API_SERVER = "https://portal.huflit.edu.vn";
 
 class APIHuflit {
-     constructor() {
-          this.jar = request.jar();
-          request = request.defaults({
-               resolveWithFullResponse: true,
-               simple: false,
-               headers: {
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.59',
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                    'accept-language': 'vi,en-US;q=0.9,en;q=0.8',
-               }
-          });
-     }
-     requestServer(data = { URI, formData: '' }) {
-          let form = {
-               uri: API_SERVER + data.URI,
-               jar: this.jar,
-               method: (typeof data.formData === 'object') ? 'post' : 'get',
-               formData: data.formData,
-               transform: (body) => cheerio.load(body)
-          }
-          return request(form);
-     }
-     login({ user, pass }) {
-          return new Promise(async(resolve, reject) => {
-               try {
-                    const $ = await this.requestServer({
-                         URI: '/Login',
-                         formData: {
-                              'txtTaiKhoan': user,
-                              'txtMatKhau': pass
-                         }
-                    });
-                    if ($('a.stylecolor span').text().indexOf(user) >= 0)
-                         resolve({ isDone: true, cookie: this.jar._jar.store.idx['portal.huflit.edu.vn']['/']['ASP.NET_SessionId'].toString(), name: $('a.stylecolor span').text() });
-                    reject({ isDone: false, msg: 'Wrong user or pass' });
-               } catch (err) {
-                    console.log(err);
-                    reject('server error');
-               }
-          })
-     }
-     getSchedules(semester) {
-          return new Promise(async(resolve, reject) => {
-               try {
-                    var Schedules = [];
-                    var $ = await this.requestServer({
-                         URI: 'Home/DrawingSchedules?YearStudy=2020-2021&TermID=' + semester + '&Week=41&t=0.5507445018467367',
-                         formData: '',
-                    });
-                    $('.Content').each(function(i, e) {
-                         Schedules.push(subjects($(this), $(this)['0'].attribs.title));
-                    });
-                    resolve(Schedules);
-               } catch (error) {
-                    reject('server error');
-               }
-          })
+	constructor() {
+		this.jar = request.jar();
+		request = request.defaults({
+			resolveWithFullResponse: true,
+			simple: false,
+			headers: {
+				"user-agent":
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.59",
+				accept:
+					"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+				"accept-language": "vi,en-US;q=0.9,en;q=0.8",
+			},
+		});
+	}
+	requestServer(data = { URI, formData: "" }) {
+		let form = {
+			uri: API_SERVER + data.URI,
+			jar: this.jar,
+			method: typeof data.formData === "object" ? "post" : "get",
+			formData: data.formData,
+			transform: (body) => cheerio.load(body),
+		};
+		return request(form);
+	}
+	login({ user, pass }) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const $ = await this.requestServer({
+					URI: "/Login",
+					formData: {
+						txtTaiKhoan: user,
+						txtMatKhau: pass,
+					},
+				});
+				if (
+					$("a.stylecolor>span:nth-child(1)")
+						.text()
+						.indexOf(user) >= 0
+				)
+					resolve({
+						isDone: true,
+						cookie: this.jar.getCookieString(API_SERVER),
+						name: $("a.stylecolor span").text(),
+					});
+				reject({ isDone: false, msg: "Wrong user or pass" });
+			} catch (err) {
+				console.log(err);
+				reject("server error");
+			}
+		});
+	}
+	getSchedules(semester) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				var Schedules = [];
+				var $ = await this.requestServer({
+					URI:
+						"Home/DrawingSchedules?YearStudy=2020-2021&TermID=" +
+						semester +
+						"&Week=41&t=0.5507445018467367",
+					formData: "",
+				});
+				$(".Content").each(function (i, e) {
+					Schedules.push(
+						subjects($(this), $(this)["0"].attribs.title)
+					);
+				});
+				resolve(Schedules);
+			} catch (error) {
+				reject("server error");
+			}
+		});
 
-          function subjects(data, day) {
-               return {
-                    Thu: day,
-                    Phong: data.children(':nth-child(1)').text(),
-                    MonHoc: data.children(':nth-child(3)').text().split(' (')[0],
-                    TietHoc: data.children(':nth-child(9)').text().split(': ')[1],
-                    GiaoVien: data.children(':nth-child(11)').text().split(': ')[1],
-               }
-          }
-     }
-     ChangePass(oldPass, newPass) {
-          return new Promise(async(resolve, reject) => {
-               try {
-                    var $ = await this.requestServer({
-                         URI: '/API/Student/auther?t=0.6284478731933405&pw=' + oldPass + '&pw1=' + newPass + '&pw2=' + newPass,
-                    });
-                    if ($.text() == 'Mật khẩu cũ không chính xác') reject($.text())
-                    resolve($.text());
-               } catch (error) {
-                    reject('server error');
-               }
-          })
-     }
-     CheckCookie() {
-          return new Promise(async(resolve, reject) => {
-               try {
-                    var $ = await this.requestServer({
-                         URI: '/Home',
-                    });
-                    return $('a.stylecolor span').text() ? resolve({ isDone: true, name: $('a.stylecolor span').text() }) : reject({ isDone: false, msg: "GetCookie" });
-
-               } catch (error) {
-                    reject('server error');
-               }
-          })
-     }
+		function subjects(data, day) {
+			return {
+				Thu: day,
+				Phong: data.children(":nth-child(1)").text(),
+				MonHoc: data
+					.children(":nth-child(3)")
+					.text()
+					.split(" (")[0],
+				TietHoc: data
+					.children(":nth-child(9)")
+					.text()
+					.split(": ")[1],
+				GiaoVien: data
+					.children(":nth-child(11)")
+					.text()
+					.split(": ")[1],
+			};
+		}
+	}
+	ChangePass(oldPass, newPass) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				var $ = await this.requestServer({
+					URI:
+						"/API/Student/auther?t=0.6284478731933405&pw=" +
+						oldPass +
+						"&pw1=" +
+						newPass +
+						"&pw2=" +
+						newPass,
+				});
+				if ($.text() == "Mật khẩu cũ không chính xác")
+					reject($.text());
+				resolve($.text());
+			} catch (error) {
+				reject("server error");
+			}
+		});
+	}
+	CheckCookie() {
+		return new Promise(async (resolve, reject) => {
+			try {
+				var $ = await this.requestServer({
+					URI: "/Home",
+				});
+				return $("a.stylecolor span").text()
+					? resolve({
+							isDone: true,
+							name: $("a.stylecolor span").text(),
+					  })
+					: reject({ isDone: false, msg: "GetCookie" });
+			} catch (error) {
+				reject("server error");
+			}
+		});
+	}
 }
 
 module.exports = APIHuflit;
