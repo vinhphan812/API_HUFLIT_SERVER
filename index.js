d09e169bc69a7ab661e3903123e8de5ce2ff8035
vinhphan812 <vinhphan812@gmail.com>
2021-03-10 00:25:42 +0700

    fix index

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express"),
-	path = require("path"),
 	cors = require("cors");
 
-const controlAPI = require("./Controller/api.controller");
+const ctrlAPI = require("./Controller/api.controller");
 
 const app = express(),
-	port = 3000;
+	port = process.env.PORT || 3000;
 
 const listAPI = require("./static/listAPI");
 
@@ -15,32 +14,18 @@ app.set("views", "static");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static("static"));
 
-app.get("/", cors(), function (req, res) {
-	res.render("HOME", { list: listAPI });
-});
+app.get("/", (req, res) => res.render("HOME", { list: listAPI }));
 
-app.post("/Login", controlAPI.loginAPI);
+app.post("/Login", ctrlAPI.loginAPI);
 
-app.post(
-	"/CheckCookie",
-	controlAPI.middlewareCheckCookie,
-	controlAPI.checkCookie
-);
+app.post("/CheckCookie", ctrlAPI.checkCookie);
 
-app.post(
-	"/GetSchedule",
-	controlAPI.middlewareCheckCookie,
-	controlAPI.getSchedule
-);
+app.post("/GetSchedule", ctrlAPI.middlewareCheckCookie, ctrlAPI.getSchedule);
 
-app.post(
-	"/ChangePass",
-	controlAPI.middlewareCheckCookie,
-	controlAPI.changePass
-);
+app.post("/ChangePass", ctrlAPI.middlewareCheckCookie, ctrlAPI.changePass);
 
-app.post("/GetMark", controlAPI.middlewareCheckCookie, controlAPI.getMark);
+app.post("/GetMark", ctrlAPI.middlewareCheckCookie, ctrlAPI.getMark);
 
-app.listen(process.env.PORT || port, () => console.log("Server is running..."));
+app.listen(port, () => console.log("Server is running..."));
